4ba49da983204eddf1fef998c561b58f4be7fe51
Prasad Dananjaya Wilagama <85976286+PrasadWilagama@users.noreply.github.com>
2023-02-22 18:15:47 +0530

    Add files via upload
    Adding OSGi Framework Files

---
+++ ServicePublisher/src/servicepublisher/ServicePublishImpl.java
@@ -0,0 +1,9 @@
+package servicepublisher;
+
+public class ServicePublishImpl implements ServicePublish{
+	@Override
+	public String publishService() {
+		return "Execute the publish service of ServicePublisher";
+	}
+
+}
